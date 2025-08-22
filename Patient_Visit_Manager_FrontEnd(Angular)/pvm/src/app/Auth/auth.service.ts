ae0a00f41422c65ef4cdbca8810d09ea42c7dc02
Dawood Nadeem <dawood.nadeem@curemd.com>
2025-08-22 09:58:48 -0400

    .

---
+++ Patient_Visit_Manager_FrontEnd(Angular)/pvm/src/app/Auth/auth.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  
+}
